db683817093153862a82c524b014ec2c419c1d79
Victor Calebe <victorcalebeacd@gmail.com>
2023-08-20 13:36:14 -0300

    feature: Adicionando Fronte end do app

---
+++ FrontEnd/index.js
@@ -0,0 +1,15 @@
+let nome_task           = document.getElementById('nome-task')
+let data_task           = document.getElementById('data-task')
+let estado_task         = document.getElementById('estado-task')
+let prioridade_task     = document.getElementById('prioridade-task')
+let categoria_task      = document.getElementById('categoria-task')
+let description_task    = document.getElementById('description-task')
+
+let todo_task_container     = document.getElementsByClassName('todo-task-container')
+let doing_task_container    = document.getElementsByClassName('doing-task-container')
+let done_task_container     = document.getElementsByClassName('done-task-container')
+
+
+function add_task(){
+    
+}
